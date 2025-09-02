3c0e44c80193514c2a16c7023160db9f5fb05c4a
SudheerVDandu <iamsudheerdandu@gmail.com>
2025-09-02 15:32:09 +0530

    Var Conv

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,9 @@
-02-variables.sh
+#!/bin/bash
+
+PERSON1=Rahim
+PERSON2=Robert
+
+echo "$PERSON1   ::  Hi $PERSON2"
+echo "$PERSON2  ::  Hi $PERSON1"
+echo "$PERSON1   ::  How are you $PERSON2"
+echo "$PERSON2  ::  Iam good $PERSON1"
