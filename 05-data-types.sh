da56a4f74c9171e0e306e45a813cc1782a178677
iamsudheerdandu <iamsudheerdandu@gmail.com>
2025-09-03 00:30:39 +0530

    datatype

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -1,1 +1,13 @@
-05-data-types.sh
+#!/bin/bash
+
+
+
+TIMESTAMP=$(date)
+echo "script exicuted at: $TIMESTAMP"
+
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1)+($NUMBER2))
+echo "sum of $NUMBER1 and $NUMBER2 is : $SUM"
