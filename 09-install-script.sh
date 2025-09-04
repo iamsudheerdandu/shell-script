604800d94e5c0fbc4d8849e5796896b6d879c0c1
iamsudheerdandu <iamsudheerdandu@gmail.com>
2025-09-04 11:14:46 +0530

    script added to install mysql

--- 09-install-script.sh
+++ 09-install-script.sh
@@ -1,31 +1,75 @@
+
+
+
+# USERID=$(id -u)
+
+# if [ $USERID -ne 0 ]
+#     then
+#     echo "Required SUDO access"
+#     exit 1
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+#     then
+
+#     dnf install mysql -y
+    
+#     if [ $? -ne 0 ]
+#     then
+#         echo " Instillation mysql...Failed"
+        
+        
+#         exit 1
+#         else
+#         echo "Instillation mysql.... Success"
+#     fi
+# else
+#     echo "MySQL already Installed"
+
+# fi
+
 #!/bin/bash
 
 
-USERID=(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-    then
-    echo "Required SUDO access"
-    exit 1
-fi
+then
+    echo "ERROR: User required root access"
+
+exit 1
+
+else
 
 dnf list installed mysql
 
 if [ $? -ne 0 ]
-    then
 
-    dnf install mysql -y
-    
-    if [ $? -ne 0 ]
-    then
-        echo " Instillation mysql...Failed"
-        
-        
-        exit 1
-        else
-        echo "Instillation mysql.... Success"
-    fi
+then 
+
+echo "ERROR: My sql already Installed"
+
+exit 1
+
 else
-    echo "MySQL already Installed"
 
+dnf install mysql -y
+
+if 
+
+[ $? -ne 0 ]
+
+then
+
+echo "MY SQL Instillation FAILED"
+
+exit 1
+
+else
+
+echo " My sql Instillation SUCCESS"
+
+fi
 fi
